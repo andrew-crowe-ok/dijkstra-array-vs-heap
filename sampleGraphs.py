874f5c469793bc4bc5a826e4b742b249a3b0f4dd
Andrew <158761650+andrew-crowe-ok@users.noreply.github.com>
2026-04-27 23:24:57 -0600

    Added sample graphs

---
+++ sampleGraphs.py
@@ -0,0 +1,33 @@
+# All sample graphs as they appear in Section 5 of Project 2.
+# Still need to generate our own DENSE_GRAPH_THREE.
+
+SPARSE_GRAPH_ONE = {'A','B','C','D','E','F'}
+SPARSE_EDGES_WEIGHTED_ONE= [
+    ('A','B',4), ('A','C',2), ('B','D',5),
+    ('C','D',1), ('D','E',3), ('E','F',2)
+]
+
+SPARSE_GRAPH_TWO = {1, 2, 3, 4, 5, 6, 7}
+SPARSE_EDGES_WEIGHTED_TWO= [
+    (1,2,3), (1,3,6), (2,4,2), (3,5,4),
+    (4,6,7), (5,7,1), (2,5,5)
+]
+
+DENSE_GRAPH_ONE = {'A','B','C','D','E'}
+DENSE_EDGES_WEIGHTED_ONE= [
+    ('A','B',2), ('A','C',5), ('A','D',1), ('A','E',4),
+    ('B','C',3), ('B','D',2), ('B','E',6), ('C','D',3),
+    ('C','E',1), ('D','E',2)
+]
+
+DENSE_GRAPH_TWO = {1, 2, 3, 4, 5, 6}
+DENSE_EDGES_WEIGHTED_TWO= [
+    (1,2,3), (1,3,2), (1,4,6), (1,5,5), (1,6,4),
+    (2,3,1), (2,4,2), (2,5,4), (2,6,7), (3,4,3),
+    (3,5,6), (3,6,5), (4,5,2), (4,6,4), (5,6,1)
+]
+
+DENSE_GRAPH_THREE = {}
+DENSE_EDGES_THREE= [
+
+]
